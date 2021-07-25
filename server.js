9236f5a62a771a921e5dd1cceee809df6d4273d1
tommylu780 <nhoksun340@gmail.com>
2021-07-26 00:54:09 +1000

    checked

--- server.js
+++ server.js
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MOGODB_URI || "mongodb://localhost/budget",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
