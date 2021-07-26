0234d1140e911f059d1ebf637fa5c660f33daeb7
tommylu780 <nhoksun340@gmail.com>
2021-07-26 13:13:38 +1000

    running offline checked

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,7 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const NAME = "budget-tracker";
+const CACHE_NAME = "data-cache-v1";
+const DATA_CACHE_NAME = NAME + CACHE_NAME;
+
 const FILES_TO_CACHE = [
   '/',
   './index.html',
